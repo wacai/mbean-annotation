a3212b7ae6ca80bb687b869aa4ff5fea28f6120c
zhongl <zhong.lunfu@gmail.com>
2015-01-25 00:01:14 +0800

    Get Try.

--- src/main/scala/com/wacai/mbean/actor/DynamicMBean.scala
+++ src/main/scala/com/wacai/mbean/actor/DynamicMBean.scala
@@ -7,32 +7,35 @@ import akka.util.Timeout
 
 import concurrent.Await
 import concurrent.duration.FiniteDuration
+import util._
 
 object DynamicMBean {
 
   def apply(actor: ActorRef, atMost: FiniteDuration, info: MBeanInfo) = new DynamicMBean {
+
     import akka.pattern.ask
 
     implicit val timeout = Timeout(atMost)
 
+
     def getAttribute(name: String) = {
-      Await.result(actor.ask(GetAttribute(name)).mapTo[AnyRef], atMost)
+      Await.result(actor.ask(GetAttribute(name)).mapTo[Try[AnyRef]], atMost) get
     }
 
     def getAttributes(names: Array[String]) = {
-      Await.result(actor.ask(GetAttributes(names)).mapTo[AttributeList], atMost)
+      Await.result(actor.ask(GetAttributes(names)).mapTo[Try[AttributeList]], atMost) get
     }
 
     def setAttributes(attributes: AttributeList) = {
-      Await.result(actor.ask(SetAttributes(attributes)).mapTo[AttributeList], atMost)
+      Await.result(actor.ask(SetAttributes(attributes)).mapTo[Try[AttributeList]], atMost) get
     }
 
     def setAttribute(attribute: Attribute) = {
-      Await.result(actor.ask(SetAttribute(attribute)), atMost)
+      Await.result(actor.ask(SetAttribute(attribute)).mapTo[Try[Unit]], atMost) get
     }
 
     def invoke(actionName: String, params: Array[AnyRef], signature: Array[String]) = {
-      Await.result(actor.ask(Invoke(actionName, params, signature)).mapTo[AnyRef], atMost)
+      Await.result(actor.ask(Invoke(actionName, params, signature)).mapTo[Try[AnyRef]], atMost) get
     }
 
     def getMBeanInfo = info
